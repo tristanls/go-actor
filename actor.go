fad738640116eaa0090b3f606435e28ad3741cf1
Tristan Slominski <tristan.slominski@gmail.com>
2013-05-26 23:50:13 +0200

    godoc update

--- actor.go
+++ actor.go
@@ -1,26 +1,68 @@
+// Go language actor framework.
+//
+// Create an actor behavior:
+//    func Print(self actor.Reference, become actor.Become, msg actor.Message) {
+//      for _, param := range msg.Params {
+//        fmt.Println(param.(string))
+//      }  
+//    }
+//
+// Create a new actor configuration:
+//    func main() {
+//      config := actor.Configuration()
+//      ...
+//
+// Create a new actor:
+//      ...
+//      printer := config.Create(Print)
+//      ...
+//
+// Send a message to an actor:
+//      ...
+//      printer <- config.CreateMessage("print me")
+//      printer <- actor.CreateMessage("print me")
+//      ...
+//
+// Wait for actor configuration to finish:
+//      ...
+//      <-config.Done
+//    }
 package actor
 
 import "fmt"
 
+// Process the next message using the Behavior given
 type Become func(Behavior)
 
+// Actors are created with behaviors. Each behavior takes a reference to "self",
+// a function to "become" another behavior, and a message to respond to.
 type Behavior func(Reference, Become, Message)
 
+// Each message is just a slice of data
 type Message struct {
   Params []interface{}
 }
 
+// Actor references is how messages are sent to actors
 type Reference chan<- Message
+
+// Send is here for completeness, use `Reference <- Message` notation instead
 func (reference Reference) Send(msg Message) {
   reference <- msg
 }
 
-type actorConfiguration struct {
+// Block on ActorConfiguration.Done to wait for all actors to finish, for example:
+//    config := actor.Configuration()
+//    // send some messages to actors
+//    <-config.Done
+type ActorConfiguration struct {
   Done chan bool
   countChan chan<- int
   messageCount int
 }
-func (configuration *actorConfiguration) Create(behavior Behavior) Reference {
+
+// Create a new actor as part of the actor configuration
+func (configuration *ActorConfiguration) Create(behavior Behavior) Reference {
   messageCounter := make(chan Message)
   reference := make(chan Message)
   go actorBehavior(*configuration, behavior, reference)
@@ -32,7 +74,8 @@ func (configuration *actorConfiguration) Create(behavior Behavior) Reference {
   return messageCounter
 }
 
-func (configuration *actorConfiguration) CreateMessage(params ...interface{}) Message {
+// CreateMessage is syntactic sugar to create a Message
+func (configuration *ActorConfiguration) CreateMessage(params ...interface{}) Message {
   message := Message{Params: make([]interface{}, len(params), len(params))}
   for index, param := range params {
     message.Params[index] = param
@@ -40,9 +83,10 @@ func (configuration *actorConfiguration) CreateMessage(params ...interface{}) Me
   return message
 }
 
-func Configuration() actorConfiguration {
+// Creates a new actor configuration
+func Configuration() ActorConfiguration {
   countChan := make(chan int)
-  configuration := actorConfiguration{Done: make(chan bool), countChan: countChan, messageCount: 0}
+  configuration := ActorConfiguration{Done: make(chan bool), countChan: countChan, messageCount: 0}
   go func() {
     for {
       i := <-countChan
@@ -62,6 +106,7 @@ func Configuration() actorConfiguration {
   return configuration
 }
 
+// CreateMessage is syntactic sugar to create a Message
 func CreateMessage(params ...interface{}) Message {
   message := Message{Params: make([]interface{}, len(params), len(params))}
   for index, param := range params {
@@ -70,7 +115,7 @@ func CreateMessage(params ...interface{}) Message {
   return message
 }
 
-func actorBehavior(configuration actorConfiguration, behavior Behavior, reference chan Message) {
+func actorBehavior(configuration ActorConfiguration, behavior Behavior, reference chan Message) {
   var become func(Behavior)
   become = func(nextBehavior Behavior) {
     become = func(Behavior){} // become can only be called once
